6670fed95776bdf09bbc587cf20f1dbf958e7fd6
Roseana11 <salesroseana@gmail.com>
2021-08-09 14:05:31 -0400

    Vigilante

---
+++ atropelamento.js
@@ -0,0 +1,221 @@
+const atropelamentos = [{
+    "quantidadeCampoGrandeAquidauana": 6,
+    "quantidadeAquidauanaMiranda": 1,
+    "quantidadeChuvosa": 5,
+    "quantidadeSeca": 2,
+    "_id": "5f0f1958271362328c9a2591",
+    "especie": {
+        "_id": "5f0f1956271362328c9a257d",
+        "nomePopular": "Gambá-de-orelha-branca",
+        "nomeCientifico": "Didelphis albiventris"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 0,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a2592",
+    "especie": {
+        "_id": "5f0f1956271362328c9a257e",
+        "nomePopular": "Gambá-de-orelha-preta",
+        "nomeCientifico": "Didelphis aurita"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 13,
+    "quantidadeAquidauanaMiranda": 6,
+    "quantidadeChuvosa": 12,
+    "quantidadeSeca": 7,
+    "_id": "5f0f1958271362328c9a2597",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2583",
+        "nomePopular": "Tamanduá-mirim",
+        "nomeCientifico": "Tamandua tetradactyla"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 42,
+    "quantidadeAquidauanaMiranda": 29,
+    "quantidadeChuvosa": 40,
+    "quantidadeSeca": 31,
+    "_id": "5f0f1958271362328c9a2598",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2584",
+        "nomePopular": "Cachorro-do-mato",
+        "nomeCientifico": "Cerdocyon thous"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 0,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a2593",
+    "especie": {
+        "_id": "5f0f1956271362328c9a257f",
+        "nomePopular": "Tatu-de-rabo-mole-comum",
+        "nomeCientifico": "Cabassous unicinctus"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 16,
+    "quantidadeAquidauanaMiranda": 4,
+    "quantidadeChuvosa": 17,
+    "quantidadeSeca": 3,
+    "_id": "5f0f1958271362328c9a2594",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2580",
+        "nomePopular": "Tatu-galinha",
+        "nomeCientifico": "Dasypus novemcinctus"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 2,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a259a",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2586",
+        "nomePopular": "Irara",
+        "nomeCientifico": "Eira barbara"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 34,
+    "quantidadeAquidauanaMiranda": 18,
+    "quantidadeChuvosa": 42,
+    "quantidadeSeca": 10,
+    "_id": "5f0f1958271362328c9a2595",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2581",
+        "nomePopular": "Tatu-peba",
+        "nomeCientifico": "Euphractus sexcinctus"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 2,
+    "quantidadeAquidauanaMiranda": 1,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 2,
+    "_id": "5f0f1958271362328c9a2599",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2585",
+        "nomePopular": "Lobo-guará",
+        "nomeCientifico": "Chrysocyon brachyurus"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 24,
+    "quantidadeAquidauanaMiranda": 12,
+    "quantidadeChuvosa": 22,
+    "quantidadeSeca": 14,
+    "_id": "5f0f1958271362328c9a2596",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2582",
+        "nomePopular": "Tamanduá-bandeira",
+        "nomeCientifico": "Myrmecophaga tridactyla"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 0,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a259b",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2587",
+        "nomePopular": "Furão-grande",
+        "nomeCientifico": "Galictis vittata"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 1,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a259f",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258b",
+        "nomePopular": "Onça-parda",
+        "nomeCientifico": "Puma concolor"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 2,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a259d",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2589",
+        "nomePopular": "Quati-de-cauda-anelada",
+        "nomeCientifico": "Nasua nasua"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 0,
+    "_id": "5f0f1958271362328c9a259c",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2588",
+        "nomePopular": "Jaguatirica",
+        "nomeCientifico": "Leopardus pardalis"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 0,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a25a1",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258d",
+        "nomePopular": "Anta",
+        "nomeCientifico": "Tapirus terrestris"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 6,
+    "quantidadeAquidauanaMiranda": 1,
+    "quantidadeChuvosa": 2,
+    "quantidadeSeca": 5,
+    "_id": "5f0f1958271362328c9a259e",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258a",
+        "nomePopular": "Mão-pelada",
+        "nomeCientifico": "Procyon cancrivorus"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 2,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 2,
+    "quantidadeSeca": 0,
+    "_id": "5f0f1958271362328c9a25a0",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258c",
+        "nomePopular": "Jaguarundi",
+        "nomeCientifico": "Puma yagouaroundi"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 0,
+    "_id": "5f0f1958271362328c9a25a2",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258e",
+        "nomePopular": "Preá",
+        "nomeCientifico": "Cavia aperea"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 1,
+    "quantidadeAquidauanaMiranda": 0,
+    "quantidadeChuvosa": 0,
+    "quantidadeSeca": 1,
+    "_id": "5f0f1958271362328c9a25a3",
+    "especie": {
+        "_id": "5f0f1956271362328c9a258f",
+        "nomePopular": "Paca",
+        "nomeCientifico": "Cuniculus paca"
+    }
+}, {
+    "quantidadeCampoGrandeAquidauana": 0,
+    "quantidadeAquidauanaMiranda": 1,
+    "quantidadeChuvosa": 1,
+    "quantidadeSeca": 0,
+    "_id": "5f0f1958271362328c9a25a4",
+    "especie": {
+        "_id": "5f0f1956271362328c9a2590",
+        "nomePopular": "Capivara",
+        "nomeCientifico": "Hydrochaerus hydrochaeris"
+    }
+}]
--- index.js
+++ index.js
@@ -1,9 +1,66 @@
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu); 
+
+
+let htmlTabela = '<table>'
+htmlTabela += `
+    <thead>
+        <tr>
+            
+            <th>DADOS</th>
+           
+        </tr>
+    </thead>
+`
+
+htmlTabela += '<tbody>'
+
 /**
- * @format
+ * Aqui eu vou injetar os dados de atropelamento
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+atropelamentos.forEach(atropelamento => {
+    htmlTabela += `
+        <tr>
+        
 
-AppRegistry.registerComponent(appName, () => App);
+        <td><img src = "img/${atropelamento.especie._id}.jpg"
+        alt = "${atropelamento.especie.nomePopular}"><br>
+        
+        <b>Espécie:</b> ${atropelamento.especie.nomePopular} (<i>${atropelamento.especie.nomeCientifico}</i>)<br>
+       <b>Quantidade de atropelamentos de Aquidauana à Campo Grande:</b> ${atropelamento.quantidadeCampoGrandeAquidauana}<br>
+       <b>Quantidade de atropelamentos de Aquidauana à Miranda:</b> ${atropelamento.quantidadeAquidauanaMiranda}<br>
+       <b>Quantidade de atropelamentos em pista chuvosa:</b> ${atropelamento.quantidadeChuvosa}<br>
+       <b> Quantidade de atropelamentos em pista seca:</b> ${atropelamento.quantidadeSeca}</td>
+            
+        </tr>
+    `
+})
+
+htmlTabela += '</tbody>'
+
+
+htmlTabela += '</table>'
+
+/**
+ * Injeta o HTML da tabela na página
+ */
+
+const divAreaDados = document.getElementById('areaDados')
+divAreaDados.innerHTML = htmlTabela
